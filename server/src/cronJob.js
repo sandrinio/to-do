d5e8eebddd4e77b83fe562e8dad73eb6acaa8123
sandrinio <sandro.suladze@gmail.com>
2018-05-09 16:08:17 +0400

    email testing

--- server/src/cronJob.js
+++ server/src/cronJob.js
@@ -1,7 +1,5 @@
 const CronJob = require('cron').CronJob;
 const Todos = require('./models/Todos')
-const dateFormat = require('dateformat');
-const now = new Date();
 const moment = require('moment');
 const mailer = require('./middleware/mailer')
 
@@ -9,7 +7,7 @@ const mailer = require('./middleware/mailer')
 
 module.exports = function (){
 	const now = moment().format('YYYY-MM-DD h:mm')
-	new CronJob('30 * * * * *', function(msg) {
+	new CronJob('30 * * * * *', function() {
 	    Todos.find({}, function (err, todoList) {
 			if(err){
 				console.log(err)
@@ -17,7 +15,9 @@ module.exports = function (){
 				todoList.forEach(function (todo) {
 					const reminder1 = moment(todo.deadline.date + ' ' + todo.deadline.time).subtract(15, 'minutes').format('YYYY-MM-DD h:mm')
 					const reminder2 = moment(todo.deadline.date + ' ' + todo.deadline.time).subtract(5, 'minutes').format('YYYY-MM-DD h:mm')
-					if(now === reminder1 || now === reminder2){
+					const deadline = moment(todo.deadline.date + ' ' + todo.deadline.time).format('YYYY-MM-DD h:mm')
+					if(now === reminder1 || now === deadline){
+							console.log(reminder1)
 						const HelperOption = {
 							from: "Todos",
 							to: 'sandro.suladze@gmail.com',
