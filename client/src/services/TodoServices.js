85e63dc993af5843eb90115fa1f2e0bd4e0d8b9b
sandrinio <sandro.suladze@gmail.com>
2018-05-09 14:22:03 +0400

    route link fix v2

--- client/src/services/TodoServices.js
+++ client/src/services/TodoServices.js
@@ -2,15 +2,15 @@ import Api from './Api'
 
 export default {
   newTask (data) {
-    return Api().post('newTask', data)
+    return Api().post('/newTask', data)
   },
   tasksGetter (id) {
-    return Api().get(`tasksGetter/${id.id}`)
+    return Api().get(`/tasksGetter/${id.id}`)
   },
   modifyTask (data) {
-    return Api().post(`modifyTask/${data[0]._id}`, data)
+    return Api().post(`/modifyTask/${data[0]._id}`, data)
   },
   deleteTask (data) {
-    return Api().post(`deleteTask/${data._id}`, data)
+    return Api().post(`/deleteTask/${data._id}`, data)
   }
 }
