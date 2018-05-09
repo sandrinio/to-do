9b6f669d1ce42707cc55a66e6528b1d1bbad8958
sandrinio <sandro.suladze@gmail.com>
2018-05-09 14:10:02 +0400

    route link fix  v1

--- client/src/services/ProjectServices.js
+++ client/src/services/ProjectServices.js
@@ -2,13 +2,13 @@ import Api from './Api'
 
 export default {
   newProject (data) {
-    return Api().post('new-project', data)
+    return Api().post('/new-project', data)
   },
   projectsGetter () {
-    return Api().get('projectsGetter')
+    return Api().get('/projectsGetter')
   },
   getProject (id) {
-    return Api().get(`getProject/${id}`)
+    return Api().get(`/getProject/${id}`)
   },
   editProject (data) {
     return Api().post(`/editProject/${data._id}`, data)
