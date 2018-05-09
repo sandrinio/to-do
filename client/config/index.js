ed614f33e0604b801223f2e42dc7e1470e4d07b8
sandrinio <sandro.suladze@gmail.com>
2018-05-09 13:17:16 +0400

    fix

--- client/config/index.js
+++ client/config/index.js
@@ -38,10 +38,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../server/src/dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../server/src/dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
