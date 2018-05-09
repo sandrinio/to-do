7e242d43605f6555b06941807182a1fd2faf5b04
sandrinio <sandro.suladze@gmail.com>
2018-05-09 21:25:18 +0400

    dateNow changes

--- server/src/cronJob.js
+++ server/src/cronJob.js
@@ -6,13 +6,13 @@ const mailer = require('./middleware/mailer')
 
 
 module.exports = function (){
-	const now = moment().format('YYYY-MM-DD h:mm')
 	new CronJob('30 * * * * *', function() {
 	    Todos.find({}, function (err, todoList) {
 			if(err){
 				console.log(err)
 			}else{
 				todoList.forEach(function (todo) {
+						const now = moment().format('YYYY-MM-DD h:mm')
 					const reminder1 = moment(todo.deadline.date + ' ' + todo.deadline.time).subtract(15, 'minutes').format('YYYY-MM-DD h:mm')
 					const reminder2 = moment(todo.deadline.date + ' ' + todo.deadline.time).subtract(5, 'minutes').format('YYYY-MM-DD h:mm')
 					const deadline = moment(todo.deadline.date + ' ' + todo.deadline.time).format('YYYY-MM-DD h:mm')
